40a45543611c9c517d74bc433db288eaccd8fe04
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-28 23:00:28 +0700

    membuat class MahasiswaContainer dengan mengunakan url,jso, dan retforfit

--- app/src/main/java/com/example/remotedatabase/ui/theme/container/MahasiswaContainer.kt
+++ app/src/main/java/com/example/remotedatabase/ui/theme/container/MahasiswaContainer.kt
@@ -1,8 +1,30 @@
 package com.example.remotedatabase.ui.theme.container
 
+import MahasiswaService
 import com.example.remotedatabase.repository.MahasiswaRepository
+import com.example.remotedatabase.repository.NetworkMahasiswaRepository
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val kontakRepository: MahasiswaRepository
 }
 
+class MahasiswaContainer : AppContainer{
+
+    private val baseUrl = "http://localhost/umyTI/"
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
