7e905e36fc4fdc015c6fdd2033e25a6332715a03
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-28 23:10:00 +0700

    Membuat dataclass InsertUiEvent dengan isi entity

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.remotedatabase.ui.viewmodel
+
+
+
+
+data class InsertUiEvent(
+    val nim: String= "",
+    val nama: String= "",
+    val alamat: String= "",
+    val jenisKelamin: String= "",
+    val kelas: String= "",
+    val angkatan: String= ""
+)
