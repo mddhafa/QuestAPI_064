3f5bb901ea4d3ea8f8d3dc2abd65753a0d419643
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-28 23:21:33 +0700

    Membuat class insertviewmodel dan menambahkan fungsi updateinsertmhsstate didalamnya

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -1,9 +1,32 @@
 package com.example.remotedatabase.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.remotedatabase.model.Mahasiswa
+import com.example.remotedatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
+class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
