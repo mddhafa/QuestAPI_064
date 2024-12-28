8497767e38c2260173c41ec6fe661788e7f89b85
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-28 22:34:24 +0700

    membuat interface AppContainer dengan variabel kontak repository dan mahasiswa

---
+++ app/src/main/java/com/example/remotedatabase/ui/theme/container/MahasiswaContainer.kt
@@ -0,0 +1,8 @@
+package com.example.remotedatabase.ui.theme.container
+
+import com.example.remotedatabase.repository.MahasiswaRepository
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+
