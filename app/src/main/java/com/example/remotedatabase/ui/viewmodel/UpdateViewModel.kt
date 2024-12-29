4c4c53fd44171c7b647f5cae5c2b4a418814960f
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-30 05:44:38 +0700

    Menambahkan UpdateViewModel untuk mengelola update mahasiswa, termasuk inisialisasi data berdasarkan NIM dan pembaruan data mahasiswa.

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/UpdateViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.remotedatabase.ui.viewmodel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.remotedatabase.repository.MahasiswaRepository
+import com.example.remotedatabase.ui.view.mahasiswa.DestinasiUpdate
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+): ViewModel(){
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswaById(_nim)
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateMhs(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
