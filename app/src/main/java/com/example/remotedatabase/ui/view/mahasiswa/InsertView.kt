a680b2535aeae4e8372d016d221ccce7333644a4
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-12-30 04:27:35 +0700

    Menambahkan fungsi EntryMhsScreen untuk form entri mahasiswa dengan navigasi dan pengelolaan state

--- app/src/main/java/com/example/remotedatabase/ui/view/mahasiswa/InsertView.kt
+++ app/src/main/java/com/example/remotedatabase/ui/view/mahasiswa/InsertView.kt
@@ -30,6 +30,48 @@ import com.example.remotedatabase.ui.viewmodel.InsertViewModel
 import com.example.remotedatabase.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+            innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch{
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
